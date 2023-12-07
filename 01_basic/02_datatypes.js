d0294ba3931d000330b3a7aa503f1d72a4d126d2
Shubh-Patil31 <142447846+Shubh-Patil31@users.noreply.github.com>
2023-12-07 07:20:50 +0000

    js

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treate all js code newer version
+
+//alert(3+3); // we are using node js not a browser
+
+console.log(3
+    +3);//code readablity should be high
+
+    let name = "shubh";
+    let age = 18;
+    let isLoggedIn = false;
+    let state;//undefined
+
+    // number => 2 to the power of 53 -1
+    // bigint
+    //string =>""
+    //boolean => true/false
+    //null => standalone value
+    //undefined
+
+    console.table([typeof name, typeof age, typeof isLoggedIn, typeof state, typeof null]);
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33abc";
+
+console.log(typeof score);
+
+let numScore = Number(score);
+console.log(typeof numScore);
+console.log(numScore);
+
+//"33"=>33
+//"33abc => NaN"
+//true => 1, false =>0
+
+//0=> false, 1=> true
+//""=> false, "shubh"=> true
